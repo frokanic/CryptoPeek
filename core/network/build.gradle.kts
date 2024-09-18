233afd1cc9333f0fa8ec59374726f0d969d3838b
frokanic <84749437+frokanic@users.noreply.github.com>
2024-09-18 09:49:16 +0300

    Setup of basic dependencies. Setup the network module's gradle file.

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.kapt")
+    id("org.jetbrains.kotlin.plugin.parcelize")
+    alias(libs.plugins.dagger.hilt.android)
+}
+
+android {
+    compileSdk = 34
+    namespace = "com.frokanic.cryptopeek.network"
+
+    defaultConfig {
+        minSdk = 24
+        targetSdk = 34
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.retrofit.logger)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
