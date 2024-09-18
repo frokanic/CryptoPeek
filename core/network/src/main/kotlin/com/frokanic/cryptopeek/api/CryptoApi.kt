2c2716c7480bb95d1d029b79927738d5348d5178
Fotis <f.rokanis@gmail.com>
2024-09-18 23:50:53 +0300

    Create the network response model. Keep some notes on the NetworkConstants. Create the CryptoApi's calls.

--- core/network/src/main/kotlin/com/frokanic/cryptopeek/api/CryptoApi.kt
+++ core/network/src/main/kotlin/com/frokanic/cryptopeek/api/CryptoApi.kt
@@ -1,4 +1,22 @@
 package com.frokanic.cryptopeek.api
 
+import com.frokanic.cryptopeek.model.CurrencyResponseModel
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface CryptoApi {
+
+    // Todo: Take notes on @Query, @Path and @Body. Especially on @Body, since the other 2 are pretty straightforward.
+    @GET("coins/markets")
+    fun getAllCrypto(
+        @Query("vs_currency") vsCurrency: String,
+    ): Response<CurrencyResponseModel>
+
+    @GET("coins/{id}")
+    fun getCoinInfo(
+        @Path("id") id: String
+    ): Response<CurrencyResponseModel>
+
 }
--- core/network/src/main/kotlin/com/frokanic/cryptopeek/util/NetworkConstants.kt
+++ core/network/src/main/kotlin/com/frokanic/cryptopeek/util/NetworkConstants.kt
@@ -2,5 +2,13 @@ package com.frokanic.cryptopeek.util
 
 object NetworkConstants {
 
-    const val BASE_URL = "https://api.coingecko.com/api/v3/"
+    /**
+     *  Properties marked with the const modifier are compile-time constants, meaning
+     *  their value is known at compile time, unlike val, where they can be assigned
+     *  at run-time.
+     *
+     *  For that reason, they can only be of String or primitive type
+     */
+    // Todo: Research and take notes on how compile-time constants are better for performance.
+    internal const val BASE_URL = "https://api.coingecko.com/api/v3/"
 }
