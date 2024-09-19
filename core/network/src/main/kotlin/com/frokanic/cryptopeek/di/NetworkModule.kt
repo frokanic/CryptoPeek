0917a03e495c81dff4da89e7302588ebf1618a7e
Fotis <f.rokanis@gmail.com>
2024-09-19 09:11:05 +0300

    Add the di code to the network module. Add an additional dependency.

--- core/network/src/main/kotlin/com/frokanic/cryptopeek/di/NetworkModule.kt
+++ core/network/src/main/kotlin/com/frokanic/cryptopeek/di/NetworkModule.kt
@@ -1,11 +1,51 @@
 package com.frokanic.cryptopeek.di
 
+import com.frokanic.cryptopeek.api.CryptoApi
+import com.frokanic.cryptopeek.util.NetworkConstants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    // Todo: Add notes on ohHttpClient, its logging ect.
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+
+        return OkHttpClient
+            .Builder()
+            .addNetworkInterceptor(
+                interceptor = loggingInterceptor
+            )
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideCryptoApi(
+        okHttpClient: OkHttpClient
+    ): CryptoApi =
+        Retrofit
+            .Builder()
+            .client(okHttpClient)
+            .baseUrl(
+                NetworkConstants.BASE_URL
+            )
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create()
 
 }
