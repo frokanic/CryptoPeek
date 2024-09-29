b554932e60c915a77b745b9ac41556c3c50c6deb
Fotis <f.rokanis@gmail.com>
2024-09-29 17:02:02 +0300

    Create a state that holds the data of the AllCurrenciesScreen.

---
+++ feature/allcrypto/src/main/kotlin/com/frokanic/cryptopeek/AllCurrenciesUiState.kt
@@ -0,0 +1,22 @@
+package com.frokanic.cryptopeek
+
+import com.frokanic.model.model.CurrencyOverview
+
+sealed interface AllCurrenciesUiState {
+
+    data object Loading : AllCurrenciesUiState
+
+    data class Success(
+        val currencies: List<CurrencyOverview>?
+    ) : AllCurrenciesUiState
+
+    data class Failure(
+        val errorDescription: String
+    ) : AllCurrenciesUiState
+
+    data class FailureWithFallback(
+        val currencies: List<CurrencyOverview>?,
+        val errorDescription: String
+    ) : AllCurrenciesUiState
+
+}
