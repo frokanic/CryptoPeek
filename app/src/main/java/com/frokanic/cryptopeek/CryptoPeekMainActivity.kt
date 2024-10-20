224312b33d211ffe94a978c6ceb52aa6d6de6753
Fotis <f.rokanis@gmail.com>
2024-10-20 20:31:05 +0300

    Rename the activity.

--- app/src/main/java/com/frokanic/cryptopeek/CryptoPeekMainActivity.kt
+++ app/src/main/java/com/frokanic/cryptopeek/CryptoPeekMainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.frokanic.cryptopeek.navigation.CryptoPeekNavGraph
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class CryptoPeekMainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            CryptoPeekNavGraph()
         }
     }
 }
