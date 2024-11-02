3b8684fe468cc649a9e4e80c12ef941770135a37
Fotis <f.rokanis@gmail.com>
2024-11-03 00:16:46 +0200

    Add navigation logic Delete old navigation files

---
+++ app/src/main/java/com/frokanic/cryptopeek/navigation/CryptoPeekNavGraph.kt
@@ -0,0 +1,35 @@
+package com.frokanic.cryptopeek.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.frokanic.cryptopeek.AllCryptoScreen
+import com.frokanic.cryptopeek.CryptoDetailsScreen
+import com.frokanic.cryptopeek.Routes
+
+@Composable
+internal fun CryptoPeekNavGraph() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.AllCrypto
+    ) {
+        composable<Routes.AllCrypto> {
+            AllCryptoScreen(
+                onClickOnItem = {
+                    navController.navigate(
+                        route = Routes.CryptoDetails(id = it)
+                    )
+                }
+            )
+        }
+
+        composable<Routes.CryptoDetails> {
+            CryptoDetailsScreen(
+                clickedCryptoId = it.id
+            )
+        }
+    }
+}
